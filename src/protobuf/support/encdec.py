f8badb08b50a3160a86805ad15a056473c16998d
Michael Haberler <git@mah.priv.at>
2013-10-12 04:06:22 +0200

    protobuf: repair encdec.py demo
    telegram doesn exist anymore

--- src/protobuf/support/encdec.py
+++ src/protobuf/support/encdec.py
@@ -27,51 +27,42 @@ origin.name = "gladevcp"
 
 container = Container()
 container.type = MT_HALUPDATE
+container.serial = 34567
+container.origin.MergeFrom(origin)
 
-report = container.telegram
-report.op = UPDATE
-report.serial = 34567
-report.rsvp = NONE
-report.origin.MergeFrom(origin)
-
-arg = report.args.add()
-arg.type = HAL__PIN
-arg.pin.type = HAL__S32
+arg = container.args.add()
+arg.type = HAL_PIN
+arg.pin.type = HAL_S32
 arg.pin.name = "foo.1.bar"
 arg.pin.hals32 = 4711
 
-print "payload:", report.ByteSize()
+print "payload:", container.ByteSize()
 print "text format:", str(container)
 
 buffer = container.SerializeToString()
 print "wire format length=%d %s" % (len(buffer), binascii.hexlify(buffer))
 
 jsonout = container.SerializeToJSON()
-print json.dumps(json.loads(jsonout), indent=4)
+print "json format:",json.dumps(json.loads(jsonout), indent=4)
 
 jsonmsg = '''
 {
-    "telegram": {
-        "origin": {
-            "origin": 10,
-            "name": "gladevcp",
-            "id": 234
-        },
-        "args": [
-            {
-                "type": 1,
-                "pin": {
-                    "type": 3,
-                    "name": "foo.1.bar",
-                    "hals32": 4711
-                }
-            }
-        ],
-        "failed_args": [],
-        "serial": 34567,
-        "op": 4020,
-        "rsvp": 0
+    "origin": {
+        "origin": 10,
+        "name": "gladevcp",
+        "id": 234
     },
+    "serial": 34567,
+    "args": [
+        {
+            "type": 1,
+            "pin": {
+                "type": 3,
+                "name": "foo.1.bar",
+                "hals32": 4711
+            }
+        }
+    ],
     "type": 8
 }
 '''
