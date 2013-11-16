54432888ff4d1764e3af1dadd8cd9afa35c81820
Michael Haberler <git@mah.priv.at>
2013-11-16 12:39:55 +0100

    python/rtapi: add some introspection

--- src/rtapi/test-rtapilog.py
+++ src/rtapi/test-rtapilog.py
@@ -2,8 +2,21 @@ import rtapi
 
 # write to RTAPI log like to a file:
 
-rtapi = rtapi.RTAPILogger(rtapi.MSG_ERR,"testdest")
-print >> rtapi, "check the rtapi log"
+log = rtapi.RTAPILogger(rtapi.MSG_ERR,"testdest")
+print >> log, "check the rtapi log"
 
 # or directly
-rtapi.write("again")
+log.write("again")
+
+print "instance=%d name=%s flavor=%d" % (rtapi.instance,
+                                         rtapi.instance_name, rtapi.flavor_id)
+print "build=%s flags=%d" % (rtapi.build_sys, rtapi.flavor_flags)
+print "flavor_name=%s" % (rtapi.flavor_name)
+print "ulapi git=%s" % (rtapi.git_tag)
+
+
+print "rt %d user %d" % (rtapi.rtlevel,rtapi.userlevel)
+
+rtapi.rtlevel = 2
+rtapi.userlevel = 3
+print "rt %d user %d" % (rtapi.rtlevel,rtapi.userlevel)
