1408d8022bfbdac1ec36cb1fa074dc97ad60700d
Michael Haberler <git@mah.priv.at>
2014-03-11 11:47:33 +0100

    spring cleaning

--- src/hal/haltalker/addgroup.py
+++
@@ -1,9 +0,0 @@
-import sys
-group = sys.argv[1]
-for line in sys.stdin:
-    line = line.rstrip('\n')
-    signame = line.replace(".","_")
-
-    print "net %s %s" % (signame,line)
-
-    print "newm %s %s" % (group,signame)
