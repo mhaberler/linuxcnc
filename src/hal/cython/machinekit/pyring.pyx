a89a3151a9bbe3d7130ba3120a1f46e6ef45d07c
Michael Haberler <git@mah.priv.at>
2014-04-28 10:52:49 +0200

    pyring wishlist

--- src/hal/cython/machinekit/pyring.pyx
+++ src/hal/cython/machinekit/pyring.pyx
@@ -8,14 +8,17 @@ from cpython.bytes cimport PyBytes_AsString, PyBytes_Size, PyBytes_FromStringAnd
 from libring cimport *
 from .hal cimport *
 
-from os import getpid
+from os import getpid  # XXX I just want use getpid(), not export it?
 
 cdef extern int lib_module_id
 cdef int comp_id = -1
 
 cdef extern from "hal_ring.h" :
+
+    # XXX this is roughly as from: http://goo.gl/Rb28IZ
+    # XXX how do I expose this?
     ctypedef struct hal_ring_t:
-        #char name[HAL_NAME_LEN + 1]
+        const char *name   # XXX a char array - is this right?
         int next_ptr
         int ring_id
         int ring_shmkey
@@ -23,10 +26,6 @@ cdef extern from "hal_ring.h" :
         unsigned ring_offset
         unsigned flags
         int handle
-        ringbuffer_t * ring
-
-    # int hal_init(const char *name)
-    # int hal_init_mode(const char *name, int mode, int userarg1, int userarg2)
 
     int hal_ring_new(const char *name, int size, int spsize, int mode)
     int hal_ring_delete(const char *name)
@@ -34,6 +33,24 @@ cdef extern from "hal_ring.h" :
     int hal_ring_detach(const char *name, ringbuffer_t *rb)
 
 
+# attach to HAL only if needed
+def pyring_hal_init():
+    global comp_id,lib_module_id
+    cdef modname
+    if (lib_module_id < 0):
+        modname = "pyring%d" % getpid()
+        comp_id = hal_init(modname)
+        hal_ready(comp_id)
+
+# module exit handler
+def pyring_exit():
+    global comp_id
+    if comp_id > -1:
+        hal_exit(comp_id)
+
+
+import atexit                 # XXX I just want use atexit(), not export it?
+atexit.register(pyring_exit)
 
 cdef class mview:
     cdef void *base
@@ -49,13 +66,38 @@ cdef class mview:
 
 cdef class Ring:
     cdef ringbuffer_t _ring
+    cdef hal_ring_t *_hal_ring
+
+    # XXX this ctor assumes a named HAL ring.
+    # can one have a second constructor which takes say a buffer or memoryview object
+    # eg for usage with a posix shm segment, all without HAL?
 
     def __cinit__(self, char *name, int size = 0, int sp_size = 0, unsigned int flags = 0):
+
+        # attach to HAL by creating a dummy halcomp - needed only for accessing HAL rings
+        if (lib_module_id < 0): # once
+            pyring_hal_init()
         if size:
             if hal_ring_new(name, size, sp_size, flags):
                 raise RuntimeError("hal_ring_new failed")
         if hal_ring_attach(name, &self._ring, &flags):
                 raise RuntimeError("hal_ring_attach failed")
+
+        # XXX ok, the HAL object exists, and the ring is attached
+        # what I would need here is to call
+        # hal_ring_t *halpr_find_ring_by_name(const char *name) to init the _hal_ring *
+        # this must happen under hal_mutex, I usually do this so in C:
+        # {
+	#    int dummy __attribute__((cleanup(halpr_autorelease_mutex)));
+	#    rtapi_mutex_get(&(hal_data->mutex));
+	#    self._hal_ring = halpr_find_ring_by_name(name)
+        #    if (self._hal_ring)
+        #       raise Bug
+        # }
+
+    # XXX a second constructor for buffer object like so?
+    # def __cinit__(self, buffer):
+    #    .....
 
     def __dealloc__(self):
         pass
@@ -170,23 +212,3 @@ cdef class BufRing:
 
     def flush(self):
         msg_write_flush(&self._ring)
-
-# module init function
-def pyring_init():
-    global comp_id,lib_module_id
-    cdef modname
-    if (lib_module_id < 0):
-        modname = "pyring%d" % getpid()
-        comp_id = hal_init(modname)
-        hal_ready(comp_id)
-
-# module exit handler
-def pyring_exit():
-    global comp_id
-    if comp_id > -1:
-        hal_exit(comp_id)
-
-pyring_init()
-
-import atexit
-atexit.register(pyring_exit)
