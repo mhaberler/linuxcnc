b7120e2736b16ff2e5a90967137564857567c282
Michael Haberler <git@mah.priv.at>
2014-06-22 10:09:34 +0200

    hal/cythone/nosetests: fix nosetest make target
    and some nosetests along the way

--- lib/python/machinekit/nosetests/realtime.py
+++ lib/python/machinekit/nosetests/realtime.py
@@ -2,23 +2,8 @@ import subprocess
 import os
 from subprocess import Popen, PIPE
 
-# def setup_module():
-#     p = Popen(["realtime", "restart"], stdout=PIPE)
-#     output = p.communicate()[0]
-#     if p.returncode:
-#         raise Exception, "realtime restart failed: " + output
-
-# def teardown_module():
-#     p = Popen(["realtime", "stop"], stdout=PIPE)
-#     output = p.communicate()[0]
-#     if p.returncode:
-#         raise Exception, "realtime stop failed: " + output
-
-
 def setup_module():
-    my_env = os.environ.copy()
-    #my_env["DEBUG"] = "5"
-    subprocess.call(["realtime", "restart"], env=my_env)
+    subprocess.call("realtime restart", shell=True,stderr=subprocess.STDOUT,env=os.environ.copy())
 
 def teardown_module():
-    subprocess.call(["realtime", "stop"])
+    subprocess.call("realtime stop", shell=True,stderr=subprocess.STDOUT,env=os.environ.copy())
