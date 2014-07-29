5de733f18e9a830e4a750d21f390614a5346de50
Michael Haberler <git@mah.priv.at>
2014-07-29 19:57:46 +0200

    nosetests/hal_streamring.py

--- nosetests/test_ring.py
+++ nosetests/test_ring.py
@@ -2,7 +2,7 @@
 import os,time,sys
 
 from nose import with_setup
-from machinekit.nosetests.realtime import setup_module #,teardown_module
+from machinekit.nosetests.realtime import setup_module ,teardown_module
 from machinekit import rtapi,hal
 
 import ConfigParser
