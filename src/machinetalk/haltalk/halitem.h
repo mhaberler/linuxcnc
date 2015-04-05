9a683c542744f66790a13fb3c52a0d1354603120
Michael Haberler <git@mah.priv.at>
2015-04-05 21:30:23 +0200

    haltalk: begin work on ring adoption
    unfinished, but aquisition based on ring flags works fine

--- src/machinetalk/haltalk/halitem.h
+++ src/machinetalk/haltalk/halitem.h
@@ -1,4 +1,5 @@
 #include <hal.h>
+#include <hal_ring.h>
 
 union halobject_union {
     hal_pin_t *pin;
@@ -7,10 +8,18 @@ union halobject_union {
     hal_comp_t *comp;
     hal_group_t *group;
     hal_member_t *member;
+    hal_ring_t *ring;
+};
+
+union hal_value_union {
+    hal_data_u *sigptr;
+    hal_data_u **pinptr;
+    ringbuffer_t *rbptr;
 };
 
 typedef struct  {
     hal_object_type type;
     union halobject_union o;
-    void *ptr; // points to raw value if there's one (usually hal_data_u *)
+    union hal_value_union v;
+    //void *ptr; // points to raw value if there's one (usually hal_data_u *)
 } halitem_t;
