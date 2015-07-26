c8a8aff52f5cf26d0f28769e03288ab29cb50dcd
Michael Haberler <git@mah.priv.at>
2015-07-27 00:10:58 +0200

    hal_accessors.h  - first pass on atomic pinops
    requires C11 (clang current or gcc > 4.70)

--- src/hal/lib/hal_accessor.h
+++ src/hal/lib/hal_accessor.h
@@ -8,11 +8,22 @@ typedef struct { hal_pin_t sp; } s32_pin_t;
 typedef struct { hal_pin_t up; } u32_pin_t;
 typedef struct { hal_pin_t fp; } float_pin_t;
 
-#define _PINSET(ptr,member,type,value)		\
-    type *vp = SHMPTR(ptr->member.data_ptr);	\
-    *vp = value;				\
-    if (unlikely(hh_get_wmb(&ptr->member.hdr)))	\
+// see https://gcc.gnu.org/onlinedocs/gcc/_005f_005fatomic-Builtins.html
+
+#define _PINSET(ptr,member,type,value)			\
+    type *vp = SHMPTR(ptr->member.data_ptr);		\
+    __atomic_store(vp, &(value), RTAPI_MEMORY_MODEL);	\
+    if (unlikely(hh_get_wmb(&ptr->member.hdr)))		\
 	rtapi_smp_wmb();
+
+
+#define _PINGET(ptr,member,type)			 \
+    if (unlikely(hh_get_rmb(&ptr->member.hdr)))		 \
+	rtapi_smp_rmb();				 \
+    type value;						 \
+    __atomic_load((type *) SHMPTR(ptr->member.data_ptr), \
+		  &value, RTAPI_MEMORY_MODEL);		 \
+    return value;
 
 static inline void set_bit_pin(bit_pin_t *p, const hal_bit_t value) {
     _PINSET(p, bp, hal_bit_t, value);
@@ -26,11 +37,6 @@ static inline void set_u32_pin(u32_pin_t *p, const hal_u32_t value) {
 static inline void set_float_pin(float_pin_t *p, const hal_float_t value) {
     _PINSET(p, fp, hal_float_t, value);
 }
-
-#define _PINGET(ptr,member,type)			\
-    if (unlikely(hh_get_rmb(&ptr->member.hdr)))		\
-	rtapi_smp_rmb();				\
-    return *(type *) SHMPTR(ptr->member.data_ptr);	\
 
 static inline hal_bit_t get_bit_pin(const bit_pin_t *p) {
     _PINGET(p, bp, hal_bit_t)
