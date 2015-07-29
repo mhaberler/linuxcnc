31993bd8167b061a9a48b880d27b2cba9437b72c
Michael Haberler <git@mah.priv.at>
2015-07-29 11:30:30 +0200

    generic failure macros for common cases
    instead of doing the same thing 12347 times

--- src/hal/lib/hal_logging.h
+++ src/hal/lib/hal_logging.h
@@ -5,29 +5,62 @@
 RTAPI_BEGIN_DECLS
 
 void hal_print_loc(const int level,
-		     const char *func,
-		     const int line,
-		     const char *topic,
-		     const char *fmt, ...)
+		   const char *func,
+		   const int line,
+		   const char *topic,
+		   const char *fmt, ...)
     __attribute__((format(printf,5,6)));
+
+// standard failure macros
+
+// use HALFAIL_RC for methods which return 0 for success or
+// a negative return code to indicate failure.
+// set _halerror to -rc, and return that
+// usage example (note - no '-' sign on EINVAL):
+//
+//   HALFAIL_RC(EINVAL, "severely botched foo=%d", foo);
+#define HALFAIL_RC(rc, fmt, ...)				\
+    do {							\
+	hal_print_loc(RTAPI_MSG_ERR,__FUNCTION__,__LINE__-1,	\
+		      "HAL error:", fmt, ## __VA_ARGS__);	\
+	_halerrno = -(rc);					\
+	return -(rc);						\
+    } while (0)
+
+// use HALFAIL_NULL for methods which return a pointer on success or
+// a NULL to indicate failure.
+// set _halerror to -rc, and return NULL
+// usage example (note - no '-' sign on EINVAL):
+//
+//   HALFAIL_NULL(EINVAL, "severely botched foo=%d", foo);
+//
+#define HALFAIL_NULL(rc, fmt, ...)				\
+    do {							\
+	hal_print_loc(RTAPI_MSG_ERR,__FUNCTION__,__LINE__-1,	\
+		      "HAL error:", fmt, ## __VA_ARGS__);	\
+	_halerrno = -(rc);					\
+	return NULL;						\
+    } while (0)
+
 
 // checking & logging shorthands
 #define HALERR(fmt, ...)					\
+    hal_print_loc(RTAPI_MSG_ERR,__FUNCTION__,__LINE__,		\
+		  "HAL error:", fmt, ## __VA_ARGS__)
+#define HALBUG(fmt, ...)				\
     hal_print_loc(RTAPI_MSG_ERR,__FUNCTION__,__LINE__,	\
-		    "HAL error:", fmt, ## __VA_ARGS__)
-#define HALBUG(fmt, ...)					\
-    hal_print_loc(RTAPI_MSG_ERR,__FUNCTION__,__LINE__,	\
-		    "HAL BUG:", fmt, ## __VA_ARGS__)
-#define HALWARN(fmt, ...)					\
-    hal_print_loc(RTAPI_MSG_WARN,__FUNCTION__,__LINE__,	\
-		    "HAL WARNING:", fmt, ## __VA_ARGS__)
-#define HALINFO(fmt, ...)					\
-    hal_print_loc(RTAPI_MSG_INFO,__FUNCTION__,__LINE__,	\
-		    "HAL info:", fmt, ## __VA_ARGS__)
+		  "HAL BUG:", fmt, ## __VA_ARGS__)
 
-#define HALDBG(fmt, ...)					\
+#define HALWARN(fmt, ...)					\
+    hal_print_loc(RTAPI_MSG_WARN,__FUNCTION__,__LINE__,		\
+		  "HAL WARNING:", fmt, ## __VA_ARGS__)
+#define HALINFO(fmt, ...)				\
+    hal_print_loc(RTAPI_MSG_INFO,__FUNCTION__,__LINE__,	\
+		  "HAL info:", fmt, ## __VA_ARGS__)
+
+#define HALDBG(fmt, ...)				\
     hal_print_loc(RTAPI_MSG_DBG,__FUNCTION__,__LINE__,	\
-		    "HAL:", fmt, ## __VA_ARGS__)
+		  "HAL:", fmt, ## __VA_ARGS__)
 
 #define HAL_ASSERT(x)						\
     do {							\
