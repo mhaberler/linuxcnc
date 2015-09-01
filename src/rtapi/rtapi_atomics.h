da3fe58de400623f4cb2d1be3fdb677cbdaea593
Michael Haberler <git@mah.priv.at>
2015-09-01 21:15:43 +0200

    mutate rtapi_mbarrier.h into rtapi_atomics.h
    add rtapi_* atomic ops needed for the ringbuffers

--- src/rtapi/rtapi_atomics.h
+++ src/rtapi/rtapi_atomics.h
@@ -15,9 +15,11 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  ********************************************************************/
+#include <ck_pr.h>
 
-#ifndef _RTAPI_MBARRIER_H
-#define _RTAPI_MBARRIER_H
+
+#ifndef _RTAPI_ATOMICS_H
+#define _RTAPI_ATOMICS_H
 
 #include "config.h" // HAVE_CK
 
@@ -26,6 +28,44 @@
 
 #ifdef HAVE_CK
 // use concurrencykit.org primitives
+
+
+static inline uint32_t rtapi_load_32(const uint32_t *target)
+{
+    return ck_pr_load_32(target);
+}
+
+static inline void rtapi_store_32(uint32_t *target, uint32_t value)
+{
+     ck_pr_store_32(target, value);
+}
+
+
+// use gcc intrinsics until the x86 flavor of ck supports 64bit ops
+#if !defined(CK_F_PR_LOAD_64)
+static inline uint64_t rtapi_load_64(const uint64_t *target)
+{
+    return __atomic_load_n(target, RTAPI_MEMORY_MODEL);
+}
+#else
+static inline uint64_t rtapi_load_64(const uint64_t *target)
+{
+    return ck_pr_load_64(target);
+}
+#endif
+
+#if !defined(CK_F_PR_INC_64)
+static inline void rtapi_inc_64(uint64_t *target)
+{
+    __atomic_add_fetch(target, 1, RTAPI_MEMORY_MODEL);
+}
+#else
+static inline void rtapi_inc_64(uint64_t *target)
+{
+    ck_pr_inc_64(target);
+}
+#endif
+
 #define	rtapi_smp_rmb() ck_pr_fence_load()
 #define	rtapi_smp_wmb() ck_pr_fence_store()
 #define	rtapi_smp_mb()  ck_pr_fence_memory()
@@ -33,9 +73,28 @@
 #else
 
 // use gcc intrinsics
+static inline uint32_t rtapi_load_32(const uint32_t *target)
+{
+    return __atomic_load_n(target, RTAPI_MEMORY_MODEL);
+}
+
+static inline void rtapi_store_32(uint32_t *target, uint32_t value)
+{
+    __atomic_store_n(target, value, RTAPI_MEMORY_MODEL);
+}
+
+static inline uint64_t rtapi_load_64(const uint64_t *target)
+{
+    return __atomic_load_n(target, RTAPI_MEMORY_MODEL);
+}
+static inline void rtapi_inc_64(uint64_t *target)
+{
+    __atomic_add_fetch(target, 1, RTAPI_MEMORY_MODEL);
+}
+
 #define	rtapi_smp_mb()  __sync_synchronize()
 #define	rtapi_smp_wmb() __sync_synchronize()
 #define	rtapi_smp_rmb() __sync_synchronize()
 #endif
 
-#endif // _RTAPI_MBARRIER_H
+#endif // _RTAPI_ATOMICS_H
