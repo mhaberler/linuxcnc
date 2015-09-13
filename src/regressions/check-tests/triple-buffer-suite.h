ee6fcdb7cfe68b8056dce8da787475bfed4edae0
Michael Haberler <git@mah.priv.at>
2015-09-13 12:55:07 +0200

    triple buffer: add multithreaded test case

--- src/regressions/check-tests/triple-buffer-suite.h
+++ src/regressions/check-tests/triple-buffer-suite.h
@@ -62,14 +62,109 @@ START_TEST(test_triple_buffer)
 }
 END_TEST
 
+#define N_TB_OPS 10000
+
+static bool tb_start;
+enum {
+    TB_WRITE_FLIP,
+    TB_SNAP_READ,
+
+};
+struct tb_test {
+    hal_u8_t *tbflag;
+    int *tb_buf;
+    int op;
+    int count;
+    int value;
+    int snapped;
+    char *name;
+};
+
+static void *test_tbop(void * arg)
+{
+    struct tb_test *t = arg;
+    if (delta) aff_iterate(&a);
+    while (!tb_start);
+        {
+	WITH_THREAD_CPUTIME_N(t->name, t->count, RES_NS);
+	while (t->value < t->count) {
+	    switch (t->op) {
+
+	    case TB_WRITE_FLIP:
+		t->tb_buf[rtapi_tb_write(t->tbflag)] = t->value;
+		rtapi_tb_flip_writer(t->tbflag);
+		t->value++;
+		break;
+
+	    case TB_SNAP_READ:
+		if (rtapi_tb_new_snap(t->tbflag)) {
+		    t->snapped++;
+		    int v = t->tb_buf[rtapi_tb_snap(t->tbflag)];
+		    // expect nondecreasing sequence of values
+		    ck_assert(v >= t->value);
+		    t->value = v;
+		}
+		break;
+	    }
+	    if (hop && (t->value % hop == 0))
+		aff_iterate(&a);
+	}
+    }
+    return NULL;
+}
+
+START_TEST(test_triple_buffer_threaded)
+{
+    int i;
+   struct tb_test tbt[] = {
+       {
+	   .tbflag = &tb,
+	   .tb_buf = tb_buf,
+	   .op = TB_WRITE_FLIP,
+	   .count =  N_TB_OPS,
+	   .value = 0,
+	   .name = "write_flip"
+       },
+       {
+	   .tbflag = &tb,
+	   .tb_buf = tb_buf,
+	   .op = TB_SNAP_READ,
+	   .count =  N_TB_OPS-1, // termination condition
+	   .value = 0,
+	   .snapped = 0,
+	   .name = "snap_read"
+       },
+    };
+    rtapi_tb_init(&tb);
+    pthread_t tids[2];
+    tb_start = false;
+    for(i = 0; i < 2; i++){
+	pthread_create(&(tids[i]), NULL, test_tbop, &tbt[i]);
+    }
+    {
+	WITH_PROCESS_CPUTIME_N("triple buffer roundtrips", N_TB_OPS , RES_NS);
+	tb_start = true;
+	for(i = 0; i < 2; i++){
+	    pthread_join(tids[i], NULL);
+	}
+    }
+    // last value read must be last value written, flipped and snapped
+    ck_assert_int_eq(tbt[0].value - 1, tbt[1].value);
+    ck_assert(tbt[1].snapped > 0);
+    ck_assert(tbt[1].snapped <=  N_TB_OPS);
+    printf("snapped = %d out of %d\n", tbt[1].snapped,  N_TB_OPS);
+}
+END_TEST
+
 Suite * triple_buffer_suite(void)
 {
     Suite *s;
     TCase *tc_core;
-    s = suite_create("triple buffer exploration");
+    s = suite_create("triple buffer tests");
 
     tc_core = tcase_create("triple buffer");
     tcase_add_test(tc_core, test_triple_buffer);
+    tcase_add_test(tc_core, test_triple_buffer_threaded);
     suite_add_tcase(s, tc_core);
 
     return s;
